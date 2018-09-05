23974eeb9861206957e26e8b8f5faf5cf3df90e4
Adam Duff <j.adam.duff@gmail.com>
2018-09-05 22:16:10 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
 
   get '/say/:number/:phrase' do
     @number = params[:number].to_i
-    @phrase = params[:phrase]
+    @phrase = params[:phrase].split('%20').join(' ')
     @print = []
 
     @number.times do
